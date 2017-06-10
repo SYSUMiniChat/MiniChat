2cdd42ef6cef4522674ed449e7d4e0662106cba0
ChenRuihuang <915245193@qq.com>
2017-06-10 08:14:41 +0800

    add chat window and adapter

---
+++ MiniChat/app/src/main/java/com/example/caitzh/minichat/crh/ChatWindowItemInformation.java
@@ -0,0 +1,23 @@
+package com.example.caitzh.minichat.crh;
+
+/**
+ * Created by Administrator on 2017/6/10 0010.
+ */
+
+public class ChatWindowItemInformation {
+    // TODO:图片形式，以及time的类型是否需要修改
+    private String username;
+    private String information;
+    private String time;
+    private ChatWindowItemInformation(String inputUsername, String inputInformation, String inputTime) {
+        this.username = inputUsername;
+        this.information = inputInformation;
+        this.time = inputTime;
+    }
+    public void setUsername(String inputUsername){this.username = inputUsername;}
+    public void setInformation(String inputInformation){this.information = inputInformation;}
+    public void setTime(String inputTime){this.time = inputTime;}
+    public String getUsername() {return this.username;}
+    public String getInformation() {return this.information;}
+    public String getTime() {return this.time;}
+}
