237501802f11d470b5c73fa591adc6fc16441bcc
cht <1012536506@qq.com>
2017-06-18 01:42:42 +0800

    登录页面添加网络访问功能

--- MiniChat/app/src/main/java/com/example/caitzh/minichat/signIn.java
+++ MiniChat/app/src/main/java/com/example/caitzh/minichat/signIn.java
@@ -1,11 +1,16 @@
 package com.example.caitzh.minichat;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +21,14 @@ import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -82,11 +95,11 @@ public class signIn extends AppCompatActivity {
         signin_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();  // 结束当前activity
-                // 登录成功后跳转到用户信息页面(后面应该改成用户页面)
-                Intent intent = new Intent(signIn.this, personalInformation.class);
-                startActivity(intent);
-                Toast.makeText(signIn.this, "登录成功", Toast.LENGTH_LONG).show();
+                if (checkHasNet(getApplicationContext())) {  // 判断当前是否有可用网络
+                    sendRequestWithHttpConnection();  // 发送Http请求
+                } else {
+                    Toast.makeText(getApplicationContext(), "当前没有可用网络", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
@@ -126,5 +139,81 @@ public class signIn extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // 判断是否有可用网络
+    private boolean checkHasNet(Context context) {
+        // 使用 ConnectivityManager 获取手机所有连接管理对象
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getApplicationContext().getSystemService(context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            // 使用 manager 获取网络连接管理的NetworkInfo对象
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if (networkInfo == null || !networkInfo.isAvailable()) {  // 是否为空或为非连接状态
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static final String url = "http://119.29.238.202:8000/login";
+    private void sendRequestWithHttpConnection() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                try {
+                    Log.i("key", "Begin the connection");
+                    // 获取一个HttpURLConnection实例化对象
+                    connection = (HttpURLConnection) ((new URL(url).openConnection()));
+                    // 设置请求方式和响应时间
+                    connection.setRequestMethod("POST");
+                    connection.setReadTimeout(8000);
+                    connection.setConnectTimeout(8000);
+
+                    // 获取登录时输入内容等参数，并将其以流的形式写入connection中
+                    String phone = miniNumber.getText().toString();
+                    String password_ = password.getText().toString();
+                    DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+                    outputStream.writeBytes("phone=" + phone + "&password=" + password_);
+
+                    // 提交到的数据转化为字符串
+                    InputStream inputStream = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    // 从返回的JSON数据中提取关键信息
+                    JSONObject result = new JSONObject(response.toString());
+                    String code = result.getString("code");
+                    String message = result.getString("message");
+                    // test
+                    Log.i("code:", code);
+                    Log.i("message", message);
+                    if (code.equals("0")) {  // 登录成功
+                        finish();  // 结束当前activity
+                        // TODO 跳转到用户信息页面(后面应该改成用户页面)
+                        Intent intent = new Intent(signIn.this, personalInformation.class);
+                        startActivity(intent);
+                        Looper.prepare();
+                        Toast.makeText(signIn.this, message, Toast.LENGTH_LONG).show();
+                        Looper.loop();
+                    } else {
+                        Looper.prepare();
+                        Toast.makeText(signIn.this, message, Toast.LENGTH_LONG).show();  // 弹出登录失败原因
+                        Looper.loop();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {  // 关闭connection
+                    if (connection != null)
+                        connection.disconnect();
+                }
+            }
+        }).start();
     }
 }
