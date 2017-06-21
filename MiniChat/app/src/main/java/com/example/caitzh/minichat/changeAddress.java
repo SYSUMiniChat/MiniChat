3c93de595af236f15d2372f308f88157af8095b2
cht <1012536506@qq.com>
2017-06-21 14:20:20 +0800

    更改用户信息页面添加网络访问功能

--- MiniChat/app/src/main/java/com/example/caitzh/minichat/changeAddress.java
+++ MiniChat/app/src/main/java/com/example/caitzh/minichat/changeAddress.java
@@ -1,22 +1,39 @@
 package com.example.caitzh.minichat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class changeAddress extends AppCompatActivity {
-
+    EditText editText;
+    Button btn_save;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_address);
 
-        final EditText editText = (EditText)findViewById(R.id.edit_change_address);
+        editText = (EditText)findViewById(R.id.edit_change_address);
+        btn_save = (Button) findViewById(R.id.save_address);
+
         Bundle bundle = this.getIntent().getExtras();
         if (bundle != null) {
             editText.setText(bundle.getString("address"));  // 默认内容为个人信息
@@ -24,19 +41,17 @@ public class changeAddress extends AppCompatActivity {
         editText.setSelection(editText.length());  // 设置光标在最后
 
         // 点击保存按钮
-        Button btn_save = (Button) findViewById(R.id.save_address);
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(changeAddress.this, personalInformation.class);
-                // 传递修改后的文字内容给个人信息页面
-                Bundle bundle1 = new Bundle();
-                bundle1.putString("address", editText.getText().toString());
-                intent.putExtras(bundle1);
-                finish();  // 结束当前activity
-                startActivity(intent);
+                if (checkHasNet(getApplicationContext())) {
+                    sendRequestWithHttpConnection();
+                } else {
+                    Toast.makeText(getApplicationContext(), "当前没有可用网络", Toast.LENGTH_LONG).show();
+                }
             }
         });
+
         // 给AppCompatActivity的标题栏上加上返回按钮
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         if(actionBar != null){
@@ -55,5 +70,76 @@ public class changeAddress extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    // 判断是否有可用网络
+    private boolean checkHasNet(Context context) {
+        // 使用 ConnectivityManager 获取手机所有连接管理对象
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getApplicationContext().getSystemService(context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            // 使用 manager 获取网络连接管理的NetworkInfo对象
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if (networkInfo == null || !networkInfo.isAvailable()) {  // 是否为空或为非连接状态
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static final String url = "http://119.29.238.202:8000/updateUser";
+    private void sendRequestWithHttpConnection() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                try {
+                    Log.i("key", "Begin the connection");
+                    // 获取一个HttpURLConnection实例化对象
+                    connection = (HttpURLConnection) ((new URL(url).openConnection()));
+                    // 设置请求方式和响应时间
+                    connection.setRequestMethod("POST");
+                    connection.setReadTimeout(8000);
+                    connection.setConnectTimeout(8000);
+
+                    Log.i("response code", connection.getResponseCode()+"");
+                    // 获取登录时输入内容等参数，并将其以流的形式写入connection中
+                    String city = editText.getText().toString();
+                    DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+                    outputStream.writeBytes("city=" + city);
+
+                    // 提交到的数据转化为字符串
+                    InputStream inputStream = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    // 从返回的JSON数据中提取关键信息
+                    JSONObject result = new JSONObject(response.toString());
+                    String code = result.getString("code");
+                    String message = result.getString("message");
+                    // test
+                    Log.i("code:", code);
+                    Log.i("message", message);
+                    if (code.equals("0")) {  // 修改成功
+                        finish();  // 结束当前activity
+                        Intent intent = new Intent(changeAddress.this, personalInformation.class);
+                        startActivity(intent);
+                    }
+                    Looper.prepare();
+                    Toast.makeText(changeAddress.this, message, Toast.LENGTH_LONG).show();
+                    Looper.loop();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {  // 关闭connection
+                    if (connection != null)
+                        connection.disconnect();
+                }
+            }
+        }).start();
     }
 }
