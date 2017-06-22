844014cb5694528fae67d57235aeb4c0f9bb351a
ChenRuihuang <915245193@qq.com>
2017-06-23 02:59:59 +0800

    在最近聊天记录信息类中添加了ID项

--- MiniChat/app/src/main/java/com/example/caitzh/minichat/crh/ChatWindowItemInformation.java
+++ MiniChat/app/src/main/java/com/example/caitzh/minichat/crh/ChatWindowItemInformation.java
@@ -1,5 +1,7 @@
 package com.example.caitzh.minichat.crh;
 
+import com.example.caitzh.minichat.userDB;
+
 /**
  * 聊天窗口每个聊天的item
  */
@@ -9,10 +11,16 @@ public class ChatWindowItemInformation {
     private String username;
     private String information;
     private String time;
-    private ChatWindowItemInformation(String inputUsername, String inputInformation, String inputTime) {
+    private String userID;
+
+    private ChatWindowItemInformation(String inputUserID, String inputUsername, String inputInformation, String inputTime) {
+        this.userID = inputUserID;
         this.username = inputUsername;
         this.information = inputInformation;
         this.time = inputTime;
+    }
+    public void setUserID(String userID) {
+        this.userID = userID;
     }
     public void setUsername(String inputUsername){this.username = inputUsername;}
     public void setInformation(String inputInformation){this.information = inputInformation;}
@@ -20,4 +28,7 @@ public class ChatWindowItemInformation {
     public String getUsername() {return this.username;}
     public String getInformation() {return this.information;}
     public String getTime() {return this.time;}
+    public String getUserID() {
+        return userID;
+    }
 }
