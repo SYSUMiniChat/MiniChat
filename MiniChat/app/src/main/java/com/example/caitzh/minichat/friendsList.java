131cb3fac640589fb9862a0d2f39816ae3d6b5a2
zhengsd <1289565858@qq.com>
2017-06-23 13:44:33 +0800

    从服务器读取好友列表

--- MiniChat/app/src/main/java/com/example/caitzh/minichat/friendsList.java
+++ MiniChat/app/src/main/java/com/example/caitzh/minichat/friendsList.java
@@ -1,22 +1,27 @@
 package com.example.caitzh.minichat;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.caitzh.minichat.MyDB.userDB;
+import com.example.caitzh.minichat.middlewares.Check;
 import com.example.caitzh.minichat.view.EditTextWithDel;
 import com.example.caitzh.minichat.view.PinyinComparator;
 import com.example.caitzh.minichat.view.PinyinUtils;
 import com.example.caitzh.minichat.view.SideBar;
 import com.example.caitzh.minichat.view.SortModel;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -36,7 +41,24 @@ public class friendsList extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends_list);
+        addTestData();
         initViews();
+    }
+
+    /**
+     * 手动添加测试数据
+     * 同时测试数据的DATETIME
+     */
+    private void addTestData() {
+        userDB db = new userDB(getBaseContext());
+        int id = 0;
+        String nickname = "test";
+        String date = "";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        for (int i = 0; i < 10; ++i, ++id) {
+            date = simpleDateFormat.format(new java.util.Date());
+            db.insert2Table(String.valueOf(id), nickname+id, date);
+        }
     }
 
     private void initViews() {
@@ -49,9 +71,26 @@ public class friendsList extends Activity {
         initEvents();
         setAdapter();
     }
+    // 插入列表数据
 
+    /**
+     * 从数据库添加数据
+     */
     private void setAdapter() {
-        SourceDateList = filledData(getResources().getStringArray(R.array.contacts));
+        userDB db = new userDB(getBaseContext());
+        int id = 0;
+        ArrayList<String> data = new ArrayList<String>();
+        for (int i = 0; i < 10; ++i, ++id) {
+            Cursor cursor = db.findOneByNumber(String.valueOf(id));
+            if (cursor.moveToFirst()) {
+                data.add(cursor.getString(cursor.getColumnIndex("nickname")));
+            } else {
+                Log.e("Read result is null", ""+i);
+            }
+        }
+        int size = data.size();
+        //SourceDateList = filledData(getResources().getStringArray(R.array.contacts));
+        SourceDateList = filledData((String[])data.toArray(new String[size]));
         Collections.sort(SourceDateList, new PinyinComparator());
         adapter = new SortAdapter(this, SourceDateList);
         sortListView.setAdapter(adapter);
@@ -148,5 +187,16 @@ public class friendsList extends Activity {
         Collections.sort(indexString);
         sideBar.setIndexText(indexString);
         return mSortList;
+    }
+    private static final String getFriendsUrl = "http://119.29.238.202:8000/getFriends";
+    private ArrayList<String> getFriends() {
+        String userId = MyCookieManager.getUserId();
+        ArrayList<String> data = new ArrayList<String>();
+        if (Check.checkHasNet(getApplicationContext())) {
+            // 发送请求获得好友列表
+        } else {
+            Toast.makeText(getApplicationContext(), "当前没有可用网络", Toast.LENGTH_LONG).show();
+        }
+        return data;
     }
 }
